3935e0047385db4aebc69d1819098f5bb7a375a8
Sadek Badawi <sadekbadawi1998@gmail.com>
2021-11-07 18:21:56 +0200

    E-Commerce

---
+++ app/src/main/java/com/sadek/myapplication/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sadek.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sadek.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
